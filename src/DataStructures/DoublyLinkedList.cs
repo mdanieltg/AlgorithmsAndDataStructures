cac9a756d643e47efb537334c14d60951db18f1f
Daniel Medrano Huerta <daniel@quicktortoise.com>
2025-06-03 07:55:43 -0600

    Refactor `DoublyLinkedList` for null safety and code clarity
    Added nullable reference types and adjusted methods to ensure null safety. Simplified logic in `AddFirst`, `AddLast`, `Clear`, and traversal methods. Improved code readability by updating variable names, loop structures, and inline conditions.

--- src/DataStructures/DoublyLinkedList.cs
+++ src/DataStructures/DoublyLinkedList.cs
@@ -4,25 +4,26 @@ namespace AlgorithmsAndDataStructures.DataStructures;
 
 public class DoublyLinkedList<T> : IEnumerable<T>
 {
-    private class Node<TNode>
-    {
-        public Node(TNode data, Node<TNode> previous, Node<TNode> next)
-        {
-            Data = data;
-            Previous = previous;
-            Next = next;
-        }
-
-        public TNode Data { get; set; }
-        public Node<TNode> Previous { get; set; }
-        public Node<TNode> Next { get; set; }
-    }
-
-    private Node<T> _head;
-    private Node<T> _tail;
+    private Node<T>? _head;
+    private Node<T>? _tail;
 
     public int Size { get; private set; }
     public bool IsEmpty => Size == 0;
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        Node<T>? traverse = _head;
+        while (traverse is not null)
+        {
+            yield return traverse.Data;
+            traverse = traverse.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
 
     public void Add(T item)
     {
@@ -31,9 +32,10 @@ public class DoublyLinkedList<T> : IEnumerable<T>
 
     public void AddFirst(T item)
     {
-        if (IsEmpty)
+        if (_head is null)
         {
-            _head = _tail = new Node<T>(item, null, null);
+            _tail = new Node<T>(item, null, null);
+            _head = _tail;
         }
         else
         {
@@ -46,82 +48,79 @@ public class DoublyLinkedList<T> : IEnumerable<T>
 
     public void AddLast(T item)
     {
-        if (IsEmpty)
+        if (_tail is null)
         {
-            _head = _tail = new Node<T>(item, null, null);
+            AddFirst(item);
+            return;
         }
-        else
-        {
-            _tail.Next = new Node<T>(item, _tail, null);
-            _tail = _tail.Next;
-        }
+
+        _tail.Next = new Node<T>(item, _tail, null);
+        _tail = _tail.Next;
 
         ++Size;
     }
 
     public void Clear()
     {
-        var traverse = _head;
-        while (traverse != null)
+        Node<T>? traverse = _head;
+        while (traverse is not null)
         {
-            var next = traverse.Next;
-            traverse.Previous = traverse.Next = null;
-            traverse.Data = default;
+            Node<T>? next = traverse.Next;
+            traverse.Previous = null;
+            traverse.Next = null;
+            traverse.Data = default!;
             traverse = next;
         }
 
-        _head = _tail = null;
+        _head = null;
+        _tail = null;
         Size = 0;
     }
 
     public T PeekFirst()
     {
         if (IsEmpty) throw new Exception("The list is empty.");
-        return _head.Data;
+        return _head!.Data;
     }
 
     public T PeekLast()
     {
         if (IsEmpty) throw new Exception("The list is empty.");
-        return _tail.Data;
+        return _tail!.Data;
     }
 
     public T RemoveFirst()
     {
-        if (IsEmpty) throw new Exception("The list is empty.");
+        if (_head is null) throw new Exception("The list is empty.");
 
         // Extract the data at the head and move
         // the head pointer forward one node
-        var data = _head.Data;
+        T data = _head.Data;
         _head = _head.Next;
         --Size;
 
-        // If the list is empty set the tail to null as well
+        // If the list is empty, set the tail to null as well
         if (IsEmpty)
-        {
             _tail = null;
-        }
 
         // Do a memory clean of the previous node
         else
-        {
-            _head.Previous = null;
-        }
+            _head!.Previous = null;
 
         return data;
     }
 
     public T RemoveLast()
     {
-        if (IsEmpty) throw new Exception("The list is empty.");
+        if (_tail is null) throw new Exception("The list is empty.");
 
         // Extract the data at the tail and move
         // the tail pointer backwards one node
-        var data = _tail.Data;
+        T data = _tail.Data;
         _tail = _tail.Previous;
         --Size;
 
-        // If the list is empty set the tail to null as well
+        // If the list is empty, set the tail to null as well
         if (IsEmpty)
         {
             _head = null;
@@ -130,7 +129,7 @@ public class DoublyLinkedList<T> : IEnumerable<T>
         // Do a memory clean of the previous node
         else
         {
-            _tail.Next = null;
+            _tail!.Next = null;
         }
 
         return data;
@@ -148,11 +147,12 @@ public class DoublyLinkedList<T> : IEnumerable<T>
         node.Previous.Next = node.Next;
 
         // Temporary store the data we want to return
-        var data = node.Data;
+        T data = node.Data;
 
         // Memory cleanup
-        node.Data = default;
-        node.Previous = node.Next = null;
+        node.Data = default!;
+        node.Previous = null;
+        node.Next = null;
 
         --Size;
 
@@ -170,17 +170,17 @@ public class DoublyLinkedList<T> : IEnumerable<T>
         // Search from the front of the list
         if (index < Size / 2)
         {
-            for (i = 0, traverse = _head; i != index; i++)
+            for (i = 0, traverse = _head!; i != index; i++)
             {
-                traverse = traverse.Next;
+                traverse = traverse!.Next!;
             }
         }
         // Search from the back of the list
         else
         {
-            for (i = Size - 1, traverse = _tail; i != index; i++)
+            for (i = Size - 1, traverse = _tail!; i != index; i++)
             {
-                traverse = traverse.Previous;
+                traverse = traverse!.Previous!;
             }
         }
 
@@ -192,25 +192,21 @@ public class DoublyLinkedList<T> : IEnumerable<T>
         // Support searching for null
         if (default(T) is null || item is null)
         {
-            for (var traverse = _head; traverse != null; traverse = traverse.Next)
+            for (Node<T>? traverse = _head; traverse != null; traverse = traverse.Next)
             {
-                if (traverse.Data is null)
-                {
-                    Remove(traverse);
-                    return true;
-                }
+                if (traverse.Data is not null) continue;
+                Remove(traverse);
+                return true;
             }
         }
-        // Search for non-null object
+        // Search for a non-null object
         else
         {
-            for (var traverse = _head; traverse != null; traverse = traverse.Next)
+            for (Node<T>? traverse = _head; traverse != null; traverse = traverse.Next)
             {
-                if (item.Equals(traverse.Data))
-                {
-                    Remove(traverse);
-                    return true;
-                }
+                if (!item.Equals(traverse.Data)) continue;
+                Remove(traverse);
+                return true;
             }
         }
 
@@ -224,7 +220,7 @@ public class DoublyLinkedList<T> : IEnumerable<T>
         // Support searching for null
         if (default(T) is null || item is null)
         {
-            for (var traverse = _head; traverse != null; traverse = traverse.Next, index++)
+            for (Node<T>? traverse = _head; traverse != null; traverse = traverse.Next, index++)
             {
                 if (traverse.Data is null)
                 {
@@ -232,10 +228,10 @@ public class DoublyLinkedList<T> : IEnumerable<T>
                 }
             }
         }
-        // Search for non-null object
+        // Search for a non-null object
         else
         {
-            for (var traverse = _head; traverse != null; traverse = traverse.Next, index++)
+            for (Node<T>? traverse = _head; traverse != null; traverse = traverse.Next, index++)
             {
                 if (item.Equals(traverse.Data))
                 {
@@ -247,23 +243,19 @@ public class DoublyLinkedList<T> : IEnumerable<T>
         return -1;
     }
 
-    public bool Contains(T item)
-    {
-        return IndexOf(item) != -1;
-    }
+    public bool Contains(T item) => IndexOf(item) > -1;
 
-    public IEnumerator<T> GetEnumerator()
+    private class Node<TNode>
     {
-        var traverse = _head;
-        while (traverse is not null)
+        public Node(TNode data, Node<TNode>? previous, Node<TNode>? next)
         {
-            yield return traverse.Data;
-            traverse = traverse.Next;
+            Data = data;
+            Previous = previous;
+            Next = next;
         }
-    }
 
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
+        public TNode Data { get; set; }
+        public Node<TNode>? Previous { get; set; }
+        public Node<TNode>? Next { get; set; }
     }
 }
